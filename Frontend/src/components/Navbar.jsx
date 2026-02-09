7938417ef511acffe57657dcb1164b508e47609c
MMaaz07 <maazmohammed077@gmail.com>
2026-02-09 11:24:38 +0530

    fileModified

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -2,55 +2,66 @@ import { Link, useNavigate } from "react-router-dom";
 import { useCart } from "../Context/CartContext";
 import { clearToken, getAccessToken } from "../utils/auth";
 
-function Navbar(){
-    const {cartItems}=useCart();
-    const navigate=useNavigate();
+function Navbar() {
+  const { cartItems } = useCart();
+  const navigate = useNavigate();
 
+  const isLoggedIn = !!getAccessToken();
 
-    const isLoggedIn= !!getAccessToken();
+  const cartCount = isLoggedIn
+    ? cartItems.reduce((total, item) => total + item.quantity, 0)
+    : 0;
 
-    if(isLoggedIn){
-        const cartCount= cartItems.reduce((total, item)=>total + item.quantity, 0);
-    }
+  const handleLogout = () => {
+    clearToken();
+    navigate("/login");
+  };
+  return (
+    <nav className="bg-white shadow-md px-6 py-6 flex justify-between items-center fixed w-full top-0 z-50">
+      <Link to="/" className="text-2xl font-bold text-gray-800">
+        🛍️ MMCart
+      </Link>
 
-    const handleLogout=()=>{
-        clearToken();
-        navigate("/login");
-    };
-    return(
-        <nav className='bg-white shadow-md px-6 py-6 flex justify-between items-center fixed w-full top-0 z-50'>
-        <Link to='/' className='text-2xl font-bold text-gray-800'>
-             🛍️ MMCart
-        </Link>
-
-        <div className='flex items-center gap-6'>
-                {/* Login/SignUp or Logout */}
-                {!isLoggedIn ? (
-                    <>
-                        <Link to='/login' className='text-gray-800 hover:text-gray-600 font-medium'>
-                            Login
-                        </Link>
-                        <Link to='/signup' className='text-gray-800 hover:text-gray-600 font-medium'>
-                            Sign Up
-                        </Link>
-                    </>
-                ) : (
-                    <button onClick={handleLogout} className='text-gray-800 hover:text-gray-600 font-medium'>
-                        Logout
-                    </button>
-                )}
-            </div>
-
-        <Link to='/cart' className='relative text-gray-800 hover:text-gray-600 font-medium'>
-                🛒 Cart
-                {cartCount > 0 && (
-                    <span className='absolute -top-2 -right-3 bg-red-500 text-white text-xs font-bold rounded-full px-2'>
-                        {/* {cartCount} */}
-                    </span>
-                )}
+      <div className="flex items-center gap-6">
+        {/* Login/SignUp or Logout */}
+        {!isLoggedIn ? (
+          <>
+            <Link
+              to="/login"
+              className="text-gray-800 hover:text-gray-600 font-medium"
+            >
+              Login
             </Link>
-        </nav>
-    )
+            <Link
+              to="/signup"
+              className="text-gray-800 hover:text-gray-600 font-medium"
+            >
+              Sign Up
+            </Link>
+          </>
+        ) : (
+          <button
+            onClick={handleLogout}
+            className="text-gray-800 hover:text-gray-600 font-medium"
+          >
+            Logout
+          </button>
+        )}
+      </div>
+
+      <Link
+        to="/cart"
+        className="relative text-gray-800 hover:text-gray-600 font-medium"
+      >
+        🛒 Cart
+        {cartCount > 0 && (
+          <span className="absolute -top-2 -right-3 bg-red-500 text-white text-xs font-bold rounded-full px-2">
+            {isLoggedIn && cartCount}
+          </span>
+        )}
+      </Link>
+    </nav>
+  );
 }
 
 export default Navbar;
