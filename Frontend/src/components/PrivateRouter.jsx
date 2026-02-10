ba4e100be4bc323b6507f0d18a767ab6f2f609bc
MMaaz07 <maazmohammed077@gmail.com>
2026-02-10 14:43:40 +0530

    undo last commit

--- Frontend/src/components/PrivateRouter.jsx
+++ Frontend/src/components/PrivateRouter.jsx
@@ -1,8 +1,7 @@
 import {Navigate, Outlet} from "react-router-dom";
 
 const isAuthenticated=()=> !!localStorage.getItem("access_token");
-const BASE_URL="https://ecommerce-backend-equf.onrender.com"
 
-export default function PrivateRouter({redirectTo=`BASE_URL/login`}){
+export default function PrivateRouter({redirectTo='/login'}){
     return isAuthenticated()? <Outlet /> : <Navigate to={redirectTo} replace />;
 };
