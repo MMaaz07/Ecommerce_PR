3a66a28dad5d66be146db6c61d41b9a911425134
MMaaz07 <maazmohammed077@gmail.com>
2026-02-10 14:41:43 +0530

    fixing login redirection issues

--- Frontend/src/components/PrivateRouter.jsx
+++ Frontend/src/components/PrivateRouter.jsx
@@ -1,7 +1,8 @@
 import {Navigate, Outlet} from "react-router-dom";
 
 const isAuthenticated=()=> !!localStorage.getItem("access_token");
+const BASE_URL="https://ecommerce-backend-equf.onrender.com"
 
-export default function PrivateRouter({redirectTo='/login'}){
+export default function PrivateRouter({redirectTo=`BASE_URL/login`}){
     return isAuthenticated()? <Outlet /> : <Navigate to={redirectTo} replace />;
 };
